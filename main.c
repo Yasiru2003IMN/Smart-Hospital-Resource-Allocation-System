5cd969df4d7c98a79b54aa5c00385930da972aa6
PC <yasirukalhara195@gmail.com>
2026-09-17 13:47:45 +0530

    Added hospital ward data

--- main.c
+++ main.c
@@ -10,5 +10,12 @@ int main()
     printf("3. Cardiology       - Rs.4500\n");
     printf("4. Neurology        - Rs.5000\n");
 
+    printf("\nHospital Wards:\n");
+
+printf("1] General Ward    - Rs.3000/day    - Capacity: 20\n");
+printf("2] Paediatric Ward - Rs.6000/day    - Capacity: 10\n");
+printf("3] Surgical Ward   - Rs.12000/day   - Capacity: 10\n");
+printf("4] ICU             - Rs.25000/day   - Capacity: 5\n");
+
     return 0;
 }
