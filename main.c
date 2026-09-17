3f31b40f7611cc07206b088d2655141ad309c8c9
PC <yasirukalhara195@gmail.com>
2026-09-17 13:40:52 +0530

    Added doctor specialty data

--- main.c
+++ main.c
@@ -6,9 +6,9 @@ int main()
     printf("Smart Hospital & Resource Allocation System\n");
      printf("\nDoctor Specialties:\n");
     printf("1. General Practice - Rs.1500\n");
-    printf("2. Paediatrics - Rs.2500\n");
-    printf("3. Cardiology - Rs.4500\n");
-    printf("4. Neurology - Rs.5000\n");
+    printf("2. Paediatrics      - Rs.2500\n");
+    printf("3. Cardiology       - Rs.4500\n");
+    printf("4. Neurology        - Rs.5000\n");
 
     return 0;
 }
