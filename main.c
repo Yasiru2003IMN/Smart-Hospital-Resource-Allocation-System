cde92b84b9d02fd0bcbdfb6db2e9aa4ba442127e
PC <yasirukalhara195@gmail.com>
2026-09-18 08:11:07 +0530

    Added patient name input

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 int main()
 {
     int bedOccupancy[4][20] = {0};
+    char patientName[50];
+
+
     printf("Smart Hospital & Resource Allocation System\n");
      printf("\nDoctor Specialties:\n");
     printf("1. General Practice - Rs.1500 - 15 min - Daily Cap: 30\n");
@@ -17,6 +20,9 @@ printf("1] General Ward    - Rs.3000/day    - Capacity: 20\n");
 printf("2] Paediatric Ward - Rs.6000/day    - Capacity: 10\n");
 printf("3] Surgical Ward   - Rs.12000/day   - Capacity: 10\n");
 printf("4] ICU             - Rs.25000/day   - Capacity: 5\n");
+
+     printf("\nPlease Enter patient Name: ");
+    scanf(" %s", &patientName);
 
     return 0;
 }
